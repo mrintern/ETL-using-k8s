031ae72e56093432f754d25a3c725fbc508a987b
mrintern <intern@DESKTOP-TBN2BKM.localdomain>
2023-06-03 14:43:47 +0100

    prepare sql statement and add database connector

--- etl-app/weather_api_to_mysql.py
+++ etl-app/weather_api_to_mysql.py
@@ -7,6 +7,32 @@ def get_weather(api_key, lat, lon):
     data = response.json()
     return data
 
+def insert_weather_data(weather_data):
+        query = "INSERT INTO weather (country, temperature) VALUES (%s, %s)"
+        values = (weather_data['sys']['country'], weather_data['main']['temp'])
+        cursor.execute(query, values)
+        cnx.commit()
+
+# for testing purposes only, delete later
+def print_table():
+    query = "SELECT * FROM weather"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+            print(row)
+
+#Establish a connection to the mysql database
+try:
+    cnx = mysql.connector.connect(
+     host="host",
+     user="user",
+     password="password",
+     database="database"
+    )
+    cursor = cnx.cursor()
+except Exception:
+    print("mysql.connector failed! could not connect to mysql database")
+
 # hardcoded for now
 api_key = 'ccb58c269cfb807b9f52f1600b47ceae'
 lat = 44.34
@@ -15,5 +41,9 @@ lon = 11.99
 try:
     weather_data = get_weather(api_key, lat, lon)
     print(weather_data)
-except NameError:
-    print("NameError! oh no!")
+    print("\n\n")
+    print(weather_data['sys']['country'])
+    print("\n\n")
+    print(weather_data['main']['temp'])
+except Exception:
+    print("get_weather function failed!")
