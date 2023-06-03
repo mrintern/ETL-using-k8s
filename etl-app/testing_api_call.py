35fd0098176c64e3e4232d7c96e66b6084ed23ab
mrintern <intern@DESKTOP-TBN2BKM.localdomain>
2023-06-03 13:32:26 +0100

    add succesful test and gitignore

---
+++ etl-app/testing_api_call.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_weather(api_key, lat, lon):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+api_key = 'ccb58c269cfb807b9f52f1600b47ceae'
+lat = 44.34
+lon = 11.99
+
+try:
+    weather_data = get_weather(api_key, lat, lon)
+    print(weather_data)
+except NameError:
+    print("NameError! oh no!")
